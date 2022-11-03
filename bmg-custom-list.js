0e623a2d77bdd89301c8f44ee0e6227f0d15230d
Leon Barth <116115776+BarthMedia@users.noreply.github.com>
2022-11-03 19:39:03 +0100

    Update bmg-custom-list.js

--- bmg-custom-list.js
+++ bmg-custom-list.js
@@ -1,14 +1,15 @@
 (() => {
     let t = '[bmg-custom-list = "month"]',
         e = '[bmg-custom-list = "category"]',
-        o = ".w-dyn-item";
-    function r(t) {
-        t.find(journeyTabCurrentTextParentSelector).remove(),
+        o = ".w-dyn-item",
+        r = '[bmg-custom-list = "journeyTabCurrentParent"]';
+    function u(t) {
+        t.find(r).remove(),
             t.each(function () {
                 $(this).prepend(`
-            <div ${journeyTabCurrentTextParentSelector.slice(0, -1).substring(1)} style="height: 0px; overflow: hidden;">
-                <div class="${journeyTabCurrentTextWrapperSelector.substring(1)}">
-                    <div ${journeyTabCurrentTextSelector.slice(0, -1).substring(1)} class="text-size-tiny">
+            <div ${r.slice(0, -1).substring(1)} style="height: 0px; overflow: hidden;">
+                <div class="${".journey_tab-current".substring(1)}">
+                    <div ${'[bmg-custom-list = "journeyTabCurrentText"]'.slice(0, -1).substring(1)} class="text-size-tiny">
                         replace.
                         <br>
                         me.
@@ -19,41 +20,41 @@
             });
     }
     function s(e, r) {
-        let s = c(e);
+        let u = l(e);
         for (
             r.find(o).each(function () {
                 let e = $(this).find(t).text();
-                for (i = 0, n = s.length; i < n; i++) s[i].name == e && s[i].eventCount++;
+                for (i = 0, n = u.length; i < n; i++) u[i].name == e && u[i].eventCount++;
             }),
                 i = 0,
-                n = s.length;
+                n = u.length;
             i < n;
             i++
         ) {
-            let u = s[i].$eventCounter,
-                a = { val: parseInt(u.text()) };
+            let s = u[i].$eventCounter,
+                a = { val: parseInt(s.text()) };
             gsap.to(a, {
                 duration: 1,
                 ease: "power4.out",
-                val: s[i].eventCount,
+                val: u[i].eventCount,
                 onUpdate: function () {
-                    u.text(a.val.toFixed(0));
+                    s.text(a.val.toFixed(0));
                 },
             });
         }
     }
-    function u(t, r, s, u) {
+    function a(t, r, u, s) {
         t.find(o).each(function () {
             let o = $(this).find(e),
-                c = o.text();
+                a = o.text();
             if (
                 (o.parent().click(function () {
-                    a(t, r, c, u);
+                    c(t, r, a, s);
                 }),
-                "month" != s)
+                "month" != u)
             ) {
                 0 == o.parent().next().length &&
-                    o.parent().parent().append(`<div bmg-custom-list="reset" data-alt="${c} reseter." style="display: inline-block; margin-left: 1.125rem; text-decoration: underline; cursor: pointer">Back to All Events</div>`);
+                    o.parent().parent().append(`<div bmg-custom-list="reset" data-alt="${a} reseter." style="display: inline-block; margin-left: 1.125rem; text-decoration: underline; cursor: pointer">Back to All Events</div>`);
                 let l = o.parent().next();
                 gsap.to(o[0], { color: "#172031", duration: 0.35 }),
                     gsap.to(o.parent()[0], { backgroundColor: "#fff", duration: 0.35 }),
@@ -63,16 +64,16 @@
                             t.find(e).each(function () {
                                 gsap.to($(this)[0], { color: "#fff", duration: 0.35 }), gsap.to($(this).parent()[0], { backgroundColor: "rgba(255, 255, 255, 0.1)", duration: 0.35 });
                             }),
-                            a(t, r, "month", u);
+                            c(t, r, "month", s);
                     });
             }
         });
     }
-    function a(t, e, o, r) {
+    function c(t, e, o, r) {
         for (t.empty(), i = 0, n = e.length; i < n; i++) ("month" == o || e[i].category == o) && t.append(e[i].$);
-        u(t, e, o, r), s(r, t);
+        a(t, e, o, r), s(r, t);
     }
-    function c(t) {
+    function l(t) {
         let e = [];
         return (
             t.find(".journey_tab-link").each(function (t) {
@@ -81,25 +82,25 @@
             e
         );
     }
-    function l(o, r) {
-        let s = c(r),
-            u = [];
+    function f(o, r) {
+        let u = l(r),
+            s = [];
         return (
             o.each(function () {
                 let o = $(this).find(t).text();
-                u.push({ $: $(this), sortNum: f(o, s), month: "" != o ? o : s[0].name, category: $(this).find(e).text() });
+                s.push({ $: $(this), sortNum: d(o, u), month: "" != o ? o : u[0].name, category: $(this).find(e).text() });
             }),
-            u.sort((t, e) => (t.sortNum > e.sortNum ? 1 : e.sortNum > t.sortNum ? -1 : 0)),
-            u
+            s.sort((t, e) => (t.sortNum > e.sortNum ? 1 : e.sortNum > t.sortNum ? -1 : 0)),
+            s
         );
     }
-    function f(t, e) {
+    function d(t, e) {
         for (i = 0, n = e.length; i < n; i++) if (e[i].name == t) return i;
         return 0;
     }
     $(".w-dyn-list").each(function () {
         let t = $(this);
-        ($list = t.find(".w-dyn-items")), ($tabsMenu = t.parent().find(".journey_tabs-menu")), r($tabs);
-        a($list, l(t.find(o), $tabsMenu), "month", $tabsMenu);
+        ($list = t.find(".w-dyn-items")), ($tabsMenu = t.parent().find(".journey_tabs-menu")), u($tabs);
+        c($list, f(t.find(o), $tabsMenu), "month", $tabsMenu);
     });
 })();
