4b9e2dd4297bd21845161ff1b2e1bc07be568e76
Leon Barth <116115776+BarthMedia@users.noreply.github.com>
2022-12-20 12:55:37 +0100

    Create quantity.js

---
+++ quantity.js
@@ -0,0 +1,23 @@
+(() => {
+    let t = '[bmg-element = "Input"]';
+    function e(t, e, n) {
+        t.text(n), e.attr("value", n);
+    }
+    $('[bmg-element = "Quantity Wrapper"]').each(function () {
+        let n = $(this),
+            m = n.find('[bmg-element = "Left"]'),
+            i = n.find('[bmg-element = "Right"]'),
+            a = n.find('[bmg-element = "Number"]'),
+            l = n.find(t);
+        l = l.length < 1 ? n.find("input") : l;
+        let g = parseFloat(i.attr("bmg-data-max")),
+            b = parseFloat(m.attr("bmg-data-min") || 0),
+            f = parseFloat(a.text());
+        m.click(() => {
+            f > b && f--, e(a, l, f);
+        }),
+            i.click(() => {
+                (isNaN(g) || f < g) && f++, e(a, l, f);
+            });
+    });
+})();
